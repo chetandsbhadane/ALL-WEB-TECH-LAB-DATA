1c844bc01d001836a9a34e1dff414411151120d4
Chetan Bhadane <69416667+chetandsbhadane@users.noreply.github.com>
2024-05-16 01:42:26 +0530

    node learnings

---
+++ Node Practice/filereadleaarning/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+// var uc = require('upper-case');
+
+http.createServer(function(req,res){
+    fs.readFile('index.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
